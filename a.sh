86b03d032a1457711e827e3f4af866d6f7f467ab
harkirat <harkirat96@gmail.com>
2025-02-02 22:48:10 +0530

    replaced ssh command

--- a.sh
+++ a.sh
@@ -1,5 +1,9 @@
-echo "apsdopasdsadko
-asdkoasdkoasdk
-asdkjadsasdad
-asdkasdkjdsj" &> out.txt
+ssh -i ~/.ssh/id_rsa_do root@64.227.147.124
+          cd week-25-ci-next-app/ && git pull
+          pnpm install
+          pnpm run build
+          pm2 restart fe-sever
+          pm2 restart http-sever
+          pm2 restart ws-sever
+
 
