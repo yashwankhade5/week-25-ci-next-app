95d14ea21bb2b9c4bba841a117915d3aaba3a8ea
harkirat <harkirat96@gmail.com>
2025-02-02 20:31:12 +0530

    Added prisma, added dependency to prisma in next app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { client } from "@repo/db/client";
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
